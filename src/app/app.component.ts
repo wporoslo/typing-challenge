ad9571e8be6bfd6c2f4bb36d682fb001999a1b02
wporoslo <wporoslo@gmail.com>
2021-01-14 21:46:31 +0100

    set up simple scaffolding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'ng-typing-challange';
+  title: string = 'Typing challange'
+  challenge: string = 'Lorem ipsum dolor amet'
+  successMsg: string = 'You won!'
 }
